fcc2b4da8f04ae6d67c1ee6eb1d3feac3f7d63e1
Arshdeep Kaur <kaura101@student.douglascollege.ca>
2020-05-22 19:36:08 -0700

    Now I add some statement in main to prompt user and to display on screen

--- MyJavaProject/src/FirstJavaClass.java
+++ MyJavaProject/src/FirstJavaClass.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -11,9 +13,35 @@ public class FirstJavaClass {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
+		Scanner in =new Scanner(System.in);
+		System.out.println("This is my main method");
+		String firstName,LastName;
+		System.out.println("Enter you First Name -->");
+		firstName=in.next();
+		in.nextLine();
+		System.out.println("Enter you Last Name -->");
+		LastName=in.next();
+		in.nextLine();
+		callingmethod();
+		System.out.println("Your First Name is: "+firstName);
+		System.out.println("Your Last Name is: "+LastName);
+		System.out.println("The sum is : "+SumMethod(25, 45.22));
+		System.out.println("------------------");
+		System.out.println(" ----End----");
+	}
+	public static void callingmethod()
+	{
+	  System.out.println("This a method called by main method");	
 	}
 
+   public static double SumMethod(int num1,double num2) {
+		
+		return (num1+num2);
+		
+	}
+	
 }
